0af7cea5853e1d166f27fa127280c57f7b243dc4
Mohamed Fathy <codetwostart@gmail.com>
2019-10-06 14:14:23 +0200

    initial commet

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int age;//variable declaration
+           age = 37;//initialization
+           int num1 = 24;//declaration and initialization
+           int a = 4,b = 3 ,c ;
+          age = 40;
+          int firstNume = 22;
+          Console.WriteLine("Your age is " + age);//concatination
+        }
+    }
+}
